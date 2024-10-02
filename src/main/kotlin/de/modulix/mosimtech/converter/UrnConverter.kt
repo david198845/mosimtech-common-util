cf46207cd6282a0d7219abaa711fa9328cb6fd97
dweyer1 <david.weyerhaeuser@conet-isb.de>
2024-10-02 23:34:27 +0200

    Update version and add Javadocs to UrnConverter
    Incremented project version from 1.0.2-SNAPSHOT to 1.0.3-SNAPSHOT. Introduced detailed Javadocs to the UrnConverter class for better clarity on its methods. Added Gradle migration settings to the project configuration.

--- src/main/kotlin/de/modulix/mosimtech/converter/UrnConverter.kt
+++ src/main/kotlin/de/modulix/mosimtech/converter/UrnConverter.kt
@@ -2,12 +2,30 @@ package de.modulix.mosimtech.converter
 
 import de.modulix.mosimtech.model.Urn
 
+
+/**
+ * UrnConverter is an abstract class providing functionalities to convert URN (Uniform Resource Name)
+ * objects to their string representation suitable for database storage and back to URN objects.
+ */
 abstract class UrnConverter {
 
+
+    /**
+     * Converts a URN object to its string representation suitable for database storage.
+     *
+     * @param attribute The URN object to be converted. Can be null.
+     * @return The string representation of the URN suitable for database storage, or null if the input URN is null.
+     */
     fun convertToDatabaseColumn(attribute: Urn?): String? {
         return attribute?.toUrnString()
     }
 
+    /**
+     * Converts a string representation of a URN (Uniform Resource Name) from the database to an Urn object.
+     *
+     * @param dbData The string representation of the URN from the database. Can be null.
+     * @return An instance of the Urn class if the string can be successfully parsed, or null if the input is null or invalid.
+     */
     fun convertToEntityAttribute(dbData: String?): Urn? {
         return dbData?.let { Urn.parse(it) }
     }
